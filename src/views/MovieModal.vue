4dcdaab1c7ba0c44b359458dd25772681cef547b
kevine243 <kevinekazadi1101@gmail.com>
2025-04-08 18:39:41 +0300

    responsive

--- src/views/MovieModal.vue
+++ src/views/MovieModal.vue
@@ -6,11 +6,11 @@
     <Dialog pt:root:class="" pt:mask:class="backdrop-blur-sm" v-model:visible="visible" modal
       :header="props.movie.title" :style="{ width: '35vw', height: '100vh' }"
       :breakpoints="{ '1199px': '80vw', '575px': '90vw' }">
-      <div class="card p-5 flex flex-col space-y-2">
+      <div class="card p-5 space-y-2">
         <img :src="`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`" alt="" class="w-full h-1/2">
         <p>{{ props.movie.release_date }}</p>
         <p>{{ props.movie.overview }}</p>
-        <p class="bg-gray-700 rounded text-white p-1 w-2/8">{{ props.movie.vote_average }} sur 10</p>
+        <p class="bg-gray-700 rounded text-white p-2 block w-28">{{ props.movie.vote_average }} sur 10</p>
       </div>
     </Dialog>
   </div>
