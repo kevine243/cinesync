6a42eecf8f958c1a080ad559d3c816d7e2b60ad1
kevine243 <kevinekazadi1101@gmail.com>
2025-04-08 18:22:20 +0300

    responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="home" class="h-screen px-6 md:px-24 lg:px-32">
     <PopularMovies />
+    <footer>
+      <div class="py-4 text-center bg-black text-red-500 font-black text-2xl">
+        <h3>CINESYNC</h3>
+      </div>
+    </footer>
   </div>
 </template>
 
