639bba9c6231142c5e60db1cd6cea98e888096f2
kevine243 <kevinekazadi1101@gmail.com>
2025-04-08 18:28:26 +0300

    responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,11 @@
 <template>
-  <div id="home" class="h-screen px-6 md:px-24 lg:px-32">
-    <PopularMovies />
-    <footer>
-      <div class="py-4 text-center bg-black text-red-500 font-black text-2xl">
-        <h3>CINESYNC</h3>
-      </div>
-    </footer>
+  <div class="min-h-screen flex flex-col">
+    <div id="home" class="flex-1 px-6 md:px-24 lg:px-32 py-20">
+      <PopularMovies />
+    </div>
   </div>
 </template>
 
 <script setup>
 import PopularMovies from '../views/PopularMovies.vue'
-
 </script>
